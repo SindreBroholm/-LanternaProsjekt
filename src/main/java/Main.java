910dd814de9d41089c9d16743887dbb24fae2a5e
Helge Rannem <helgerannem@gmail.com>
2020-08-14 16:13:17 +0200

    Added String print

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,5 +13,24 @@ public class Main {
         terminal.putCharacter('A');
         terminal.flush();
 
+        /*for (int column = 4; column < 10; column++) {
+            terminal.setCursorPosition(column, 4);
+            terminal.putCharacter('X');
+        }
+        //row = x posisjon, setCursorPosistion første parameter = y posisjon
+
+        for (int row = 4; row < 10; row++) {
+            terminal.setCursorPosition(4, row);
+            terminal.putCharacter('X');
+        }
+*/
+        String str = "This is a String printed out in Lanterna by iterating over the characters";
+        for (int i = 0; i < str.length(); i++) {
+            terminal.setCursorPosition(i, 3);
+            terminal.putCharacter(str.charAt(i));
+        }
+
+        terminal.flush();
+
     }
 }
