cd4a107594ea1ab5144e4263721d6cde9bc7a931
Raymond Elde <raymond_elde@hotmail.com>
2020-08-17 13:34:53 +0200

    oppgave 9, -fjerning av forige posisjon

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,9 @@ public class Main {
         DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
         Terminal terminal = defaultTerminalFactory.createTerminal();
 
+        boolean continueReadingInput = true;
+
+
 /*        terminal.setCursorPosition(1, 1);
         terminal.putCharacter('A');
         terminal.flush();*/
@@ -26,25 +29,57 @@ public class Main {
             terminal.putCharacter('X');
         }
 */
-        String str = "This is a String printed out in Lanterna by iterating over the characters";
-        for (int i = 0; i < str.length(); i++) {
-            terminal.setCursorPosition(i, 3);
-            terminal.putCharacter(str.charAt(i));
-        }
+//        String str = "This is a String printed out in Lanterna by iterating over the characters";
+//        for (int i = 0; i < str.length(); i++) {
+//            terminal.setCursorPosition(i, 3);
+//            terminal.putCharacter(str.charAt(i));
+//        }
 
-        terminal.flush();
+//        terminal.flush();
 
-        KeyStroke keyStroke = null;
+        int x = 10;
+        int y = 10;
+        final char player = 'X';
+
         do {
-            Thread.sleep(5); // might throw InterruptedException
-            keyStroke = terminal.pollInput();
-        } while (keyStroke == null);
 
-        KeyType type = keyStroke.getKeyType();
-        Character c = keyStroke.getCharacter(); // used Character, not char because it might be null
+            KeyStroke keyStroke = null;
+            do {
+                Thread.sleep(5); // might throw InterruptedException
+                keyStroke = terminal.pollInput();
+            } while (keyStroke == null);
 
-        System.out.println(c);
-        terminal.flush();
+            terminal.setCursorPosition(y,x);
+            terminal.putCharacter(player);
 
+            KeyType type = keyStroke.getKeyType();
+            Character c = keyStroke.getCharacter(); // used Character, not char because it might be null
+
+            if (c == Character.valueOf('q')) {
+                continueReadingInput = false;
+                System.out.println("quit");
+                terminal.close();
+            }
+
+            switch (type){
+                case ArrowUp:
+                    x--;
+                    break;
+                case ArrowDown:
+                    x++;
+                    break;
+                case ArrowLeft:
+                    y--;
+                    break;
+                case ArrowRight:
+                    y++;
+                    break;
+            }
+            terminal.setCursorVisible(false);
+            System.out.println(c);
+            terminal.flush();
+
+
+        } while (continueReadingInput);
     }
 }
